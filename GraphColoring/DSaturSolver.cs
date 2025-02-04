7d4c3323381ebad06e1c1265ce21c0178b6af229
pederitoO <150345108+pederitoO@users.noreply.github.com>
2025-02-04 21:00:11 +0100

    Update DSaturSolver.cs

--- GraphColoring/DSaturSolver.cs
+++ GraphColoring/DSaturSolver.cs
@@ -1,180 +1,116 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using Sudoku.Shared;
 
 namespace GraphColoring
 {
-    public class DSaturSolver : ISudokuSolver
+    // Représente un nœud dans un graphe, qui peut être connecté à d'autres nœuds.
+    public class Node
     {
-        private SudokuConnections sudokuGraph;
-        private int[][] mappedGrid;
-        private const int MaxAttempts = 1000000; // Limite pour éviter les boucles infinies
+        // Identifiant unique du nœud.
+        public int Id { get; private set; }
+        // Donnée stockée dans le nœud, peut être utilisée pour diverses applications.
+        public int Data { get; private set; }
+        // Stocke les connexions aux autres nœuds et les poids de ces connexions.
+        private Dictionary<int, int> ConnectedTo;
 
-        public DSaturSolver()
+        // Initialise un nouveau nœud avec un identifiant et une donnée facultative.
+        public Node(int idx, int data = 0)
         {
-            sudokuGraph = new SudokuConnections();
-            mappedGrid = GetMappedMatrix();
+            Id = idx;
+            Data = data;
+            ConnectedTo = new Dictionary<int, int>();
         }
 
-        public SudokuGrid Solve(SudokuGrid s)
+        // Établit une connexion entre ce nœud et un autre nœud avec un poids de connexion spécifique.
+        public void AddNeighbour(Node neighbour, int weight = 0)
         {
-            var board = new int[9, 9];
-            for (int i = 0; i < 9; i++)
-                for (int j = 0; j < 9; j++)
-                    board[i, j] = s.Cells[i, j];
-
-            if (SolveDSaturWithBacktracking(board))
+            if (!ConnectedTo.ContainsKey(neighbour.Id))
             {
-                for (int i = 0; i < 9; i++)
-                    for (int j = 0; j < 9; j++)
-                        s.Cells[i, j] = board[i, j];
+                ConnectedTo[neighbour.Id] = weight;
             }
-
-            return s;
         }
 
-        private bool SolveDSaturWithBacktracking(int[,] board)
+        // Modifie la donnée stockée dans ce nœud.
+        public void SetData(int data)
         {
-            var (row, col) = FindEmptyCell(board);
-            if (row == -1 && col == -1) return true;
+            Data = data;
+        }
 
-            var possibleValues = GetPossibleValues(board, row, col);
-            foreach (var value in possibleValues)
+        // Renvoie les identifiants de tous les nœuds connectés à ce nœud.
+        public IEnumerable<int> GetConnections()
+        {
+            return ConnectedTo.Keys;
+        }
+
+        // Retourne le poids de la connexion avec un nœud spécifique.
+        public int GetWeight(Node neighbour)
+        {
+            return ConnectedTo[neighbour.Id];
+        }
+    }
+
+    // Représente un graphe, une collection de nœuds qui peuvent être interconnectés.
+    public class Graph
+    {
+        // Nombre total de nœuds dans le graphe.
+        public int TotalV { get; private set; }
+        // Stocke tous les nœuds du graphe, accessible par leur identifiant.
+        private Dictionary<int, Node> AllNodes;
+
+        // Crée un nouveau graphe vide.
+        public Graph()
+        {
+            TotalV = 0;
+            AllNodes = new Dictionary<int, Node>();
+        }
+
+        // Ajoute un nœud au graphe et renvoie le nœud ajouté ou null si un nœud avec cet identifiant existe déjà.
+        public Node? AddNode(int idx)
+        {
+            if (AllNodes.ContainsKey(idx))
+                return null;
+
+            TotalV++;
+            var node = new Node(idx);
+            AllNodes[idx] = node;
+            return node;
+        }
+
+        // Ajoute ou met à jour la donnée associée à un nœud spécifique.
+        public void AddNodeData(int idx, int data)
+        {
+            if (AllNodes.ContainsKey(idx))
             {
-                if (IsSafe(board, row, col, value))
-                {
-                    board[row, col] = value;
-                    
-                    if (SolveDSaturWithBacktracking(board))
-                        return true;
-                    
-                    board[row, col] = 0;
-                }
+                AllNodes[idx].SetData(data);
+            }
+        }
+
+        // Crée une connexion bidirectionnelle entre deux nœuds spécifiés avec un poids facultatif.
+        public void AddEdge(int src, int dst, int wt = 0)
+        {
+            AllNodes[src].AddNeighbour(AllNodes[dst], wt);
+            AllNodes[dst].AddNeighbour(AllNodes[src], wt);
+        }
+
+        // Vérifie si deux nœuds sont directement connectés.
+        public bool IsNeighbour(int u, int v)
+        {
+            if (u >= 1 && u <= 81 && v >= 1 && v <= 81 && u != v)
+            {
+                return AllNodes[u].GetConnections().Contains(v);
             }
             return false;
         }
 
-        private (int row, int col) FindEmptyCell(int[,] board)
+        // Récupère un nœud du graphe par son identifiant, renvoie null si aucun nœud avec cet identifiant n'existe.
+        public Node? GetNode(int idx)
         {
-            int maxSaturation = -1;
-            int selectedRow = -1;
-            int selectedCol = -1;
-
-            for (int row = 0; row < 9; row++)
-            {
-                for (int col = 0; col < 9; col++)
-                {
-                    if (board[row, col] == 0)
-                    {
-                        int saturation = CalculateSaturation(board, row, col);
-                        if (saturation > maxSaturation)
-                        {
-                            maxSaturation = saturation;
-                            selectedRow = row;
-                            selectedCol = col;
-                        }
-                    }
-                }
-            }
-            return (selectedRow, selectedCol);
+            return AllNodes.ContainsKey(idx) ? AllNodes[idx] : null;
         }
 
-        private int CalculateSaturation(int[,] board, int row, int col)
+        // Renvoie les identifiants de tous les nœuds présents dans le graphe.
+        public IEnumerable<int> GetAllNodesIds()
         {
-            var usedValues = new HashSet<int>();
-            
-            // Check row
-            for (int c = 0; c < 9; c++)
-                if (board[row, c] != 0)
-                    usedValues.Add(board[row, c]);
-
-            // Check column
-            for (int r = 0; r < 9; r++)
-                if (board[r, col] != 0)
-                    usedValues.Add(board[r, col]);
-
-            // Check 3x3 box
-            int boxRow = row - row % 3;
-            int boxCol = col - col % 3;
-            for (int r = boxRow; r < boxRow + 3; r++)
-                for (int c = boxCol; c < boxCol + 3; c++)
-                    if (board[r, c] != 0)
-                        usedValues.Add(board[r, c]);
-
-            return usedValues.Count;
-        }
-
-        private List<int> GetPossibleValues(int[,] board, int row, int col)
-        {
-            var used = new bool[10];
-            
-            // Check row
-            for (int c = 0; c < 9; c++)
-                if (board[row, c] != 0)
-                    used[board[row, c]] = true;
-
-            // Check column
-            for (int r = 0; r < 9; r++)
-                if (board[r, col] != 0)
-                    used[board[r, col]] = true;
-
-            // Check 3x3 box
-            int boxRow = row - row % 3;
-            int boxCol = col - col % 3;
-            for (int r = boxRow; r < boxRow + 3; r++)
-                for (int c = boxCol; c < boxCol + 3; c++)
-                    if (board[r, c] != 0)
-                        used[board[r, c]] = true;
-
-            var possibleValues = new List<int>();
-            for (int i = 1; i <= 9; i++)
-                if (!used[i])
-                    possibleValues.Add(i);
-
-            return possibleValues;
-        }
-
-        private bool IsSafe(int[,] board, int row, int col, int num)
-        {
-            // Check row
-            for (int c = 0; c < 9; c++)
-                if (board[row, c] == num)
-                    return false;
-
-            // Check column
-            for (int r = 0; r < 9; r++)
-                if (board[r, col] == num)
-                    return false;
-
-            // Check 3x3 box
-            int boxRow = row - row % 3;
-            int boxCol = col - col % 3;
-            for (int r = boxRow; r < boxRow + 3; r++)
-                for (int c = boxCol; c < boxCol + 3; c++)
-                    if (board[r, c] == num)
-                        return false;
-
-            return true;
-        }
-
-        private int[][] GetMappedMatrix()
-        {
-            var matrix = new int[9][];
-            for (int i = 0; i < 9; i++)
-            {
-                matrix[i] = new int[9];
-            }
-
-            int count = 1;
-            for (int rows = 0; rows < 9; rows++)
-            {
-                for (int cols = 0; cols < 9; cols++)
-                {
-                    matrix[rows][cols] = count++;
-                }
-            }
-            return matrix;
+            return AllNodes.Keys;
         }
     }
 }
